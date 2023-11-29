fd3812c8121195a6a7a5d1866bc61571b060b3a8
mdmehedi060 <mdmehedi060@gmail.com>
2023-11-29 06:35:15 +0600

    pinged comment

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const userCollection = client.db('madicalDb').collection('users');
     const testimonileCollection = client.db('madicalDb').collection('testimonile');
@@ -337,8 +337,8 @@ app.get('/payments/:email', async (req, res) => {
 //   })
 // })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
